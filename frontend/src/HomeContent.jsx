e7dfe708a49c71292f286d49b3af91e97d9ceaef
Vikram A <76442734+vkrm03@users.noreply.github.com>
2024-06-30 21:28:25 +0530

    Update HomeContent.jsx

--- frontend/src/HomeContent.jsx
+++ frontend/src/HomeContent.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import "./assets/HomeContent.css";
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 const HomeContent = () => {
+    const history = useHistory();
     return (
         <div className="content">
             <div className="text-content">
                 <p>Are you hungry?</p>
                 <h1>Don't Wait !</h1>
-                <a href="/menu"><button>Order Now</button></a>
+                <a><button onClick={() => history.push("/menu")}>Order Now</button></a>
             </div>
             <div className="img-content">
                 <img src="/Pizza_img.png" alt="Pizza" />
