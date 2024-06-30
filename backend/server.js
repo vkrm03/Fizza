01caff8b73c89629cc2cf84eb65405b1aa6f50ef
Vikram A <76442734+vkrm03@users.noreply.github.com>
2024-06-30 22:28:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const dotenv = require("dotenv");
+const path = require('path');
 
 dotenv.config();
 const app = express();
@@ -146,6 +147,13 @@ app.post('/cart', async (req, res) => {
   } catch (error) {
     res.sendStatus(404);
   }
+});
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 5000, () => {
